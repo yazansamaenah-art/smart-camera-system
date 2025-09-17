5b4ae86560af9fd33060c7b48ae6b3810bafa6f4
yazansamaenah-art <yazansamaenah@gmail.com>
2025-09-17 00:34:39 -0700

    Delete cloud directory

--- cloud/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-class Location(BaseModel):
-    lat: float; lon: float
-class EventIn(BaseModel):
-    timestamp: float
-    type: str
-    location: Location
-    payload: dict
-    source: str = 'edge'
