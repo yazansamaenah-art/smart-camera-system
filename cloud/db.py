f2c212109ac9235334130168b202bc246e04e102
yazansamaenah-art <yazansamaenah@gmail.com>
2025-09-17 00:00:37 -0700

    Delete cloud directory

--- cloud/db.py
+++
@@ -1,8 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from .models import Base
-DATABASE_URL=os.getenv('DATABASE_URL','sqlite:///./smartpole.db')
-engine=create_engine(DATABASE_URL, echo=False, future=True)
-SessionLocal=sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
-def init_db(): Base.metadata.create_all(bind=engine)
